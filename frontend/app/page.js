6d9a92ce5544bfa4cbb376ba1fa9adb4ca5ef7ad
Ajay Chauhan <92268333+black-devil-369@users.noreply.github.com>
2024-02-04 03:37:24 +0000

    pages components

---
+++ frontend/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="header">
+        <h1 className="font">Welcome to the Webpage</h1>
+      </div>
+    </main>
+  );
+}
