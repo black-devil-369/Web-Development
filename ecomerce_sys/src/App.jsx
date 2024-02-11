e2d37b345c5fd0e8e8def7d8224b80125bec9144
Ajay Chauhan <92268333+black-devil-369@users.noreply.github.com>
2024-02-11 07:04:17 +0000

    main app file

---
+++ ecomerce_sys/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className='head'>
+      <header className='Htop'>Welcome to mysite</header>
+    </div>
+    </>
+  )
+}
+
+export default App
